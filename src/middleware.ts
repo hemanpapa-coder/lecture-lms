9efab2d2148c8eecd1338bcb8f7f38d687281d5a
Hemanpapa <hemanpapa@gmail.com>
2026-06-26 13:40:28 +0900

    Redirect Vercel LMS traffic to local LMS

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,13 @@
-import { type NextRequest } from 'next/server'
+import { NextResponse, type NextRequest } from 'next/server'
 import { updateSession } from '@/utils/supabase/middleware'
 
+const LOCAL_LMS_URL = 'https://neuracoust.tplinkdns.com/lms'
+
 export async function middleware(request: NextRequest) {
+    if (process.env.VERCEL === '1') {
+        return NextResponse.redirect(LOCAL_LMS_URL, 307)
+    }
+
     return await updateSession(request)
 }
 
